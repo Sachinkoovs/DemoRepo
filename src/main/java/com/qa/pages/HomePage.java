5bfea87255dd3da4f1f975b15fb55890bfbc9bef
Sachinkoovs <intern.sachin@koovs.com>
2018-12-10 16:27:22 +0530

    added search page

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -1,5 +1,12 @@
 package com.qa.pages;
 
 public class HomePage {
+	
+	public void  homePage() {
+		System.out.println("homePage");
+	}
 
+	public void home() {
+		System.out.println(" home");
+	}
 }
